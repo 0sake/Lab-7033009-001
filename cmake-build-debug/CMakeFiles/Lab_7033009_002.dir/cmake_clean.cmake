9dad662b651d8331033c7bf4f7d9fe0d8ef492d8
0sake <andrea.tatini22@gmail.com>
2022-05-19 16:56:04 +0200

    .

---
+++ cmake-build-debug/CMakeFiles/Lab_7033009_002.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_7033009_002.dir/main.cpp.obj"
+  "Lab_7033009_002.exe"
+  "Lab_7033009_002.exe.manifest"
+  "Lab_7033009_002.pdb"
+  "libLab_7033009_002.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_7033009_002.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Lab_7033009_002.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lab_7033009_002.
+# This may be replaced when dependencies are built.
